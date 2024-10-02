e6db0567ce3ef77864341230771a0601e48bf726
Fukkusu <149557634+fukkusu@users.noreply.github.com>
2024-10-02 15:57:34 +0900

    scaffold UI

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 This is a [T3 Stack](https://create.t3.gg/) project bootstrapped with `create-t3-app`.
 
 ## TODO
-- [ ] Make it deploy (vercel)
-- [ ] Scaffold basic ui with mock data
+- [x] Make it deploy (vercel)
+- [x] Scaffold basic ui with mock data
 - [ ] Actually set up a database (vercel postgres)
 - [ ] Attach database to UI
 - [ ] Add authentication (w/ clerk)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,13 +19,12 @@ const mockImages = mockUrls.map((url, index) => ({
 export default function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">{
-        mockImages.map((image) => (
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
           <div key={image.id} className="w-48  p-4">
             <img src={image.url} alt="image" />
           </div>
-        ))
-        }
+        ))}
       </div>
     </main>
   );
